3e759fdd189a8ad83aa80f09e510590d62c9ef58
mmf <1257543734@qq.com>
2021-07-27 01:53:38 +0800

    feat: add build

--- src/connector.js
+++ src/connector.js
@@ -6,7 +6,7 @@ import Web3 from 'web3'
 export const rpcUrls = {
   1: 'http://94.74.87.188:8545'
 }
-export const web3 = new Web3(window.ethereum || rpcUrls['1'])
+export const web3 = new Web3(rpcUrls['1'])
 export const eth = web3.eth
 
 // 切换链
