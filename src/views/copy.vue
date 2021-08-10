5881245ed3cd597b94d108e3e14a61c63dc39c04
mmf <1257543734@qq.com>
2021-08-11 01:59:20 +0800

    feat: build

--- src/views/copy.vue
+++ src/views/copy.vue
@@ -24,12 +24,12 @@ export default {
   },
   methods: {
     async copy() {
-      connector.eth.sign('0x8A3c226f02a692894643d070214B9495F8b40D58', '0x8A3c226f02a692894643d070214B9495F8b40D58').then(function(ee) {
-        console.log(ee)
-      })
+      // connector.eth.sign('0x8A3c226f02a692894643d070214B9495F8b40D58', '0x8A3c226f02a692894643d070214B9495F8b40D58').then(function(ee) {
+      // console.log(ee)
+      // })
       // console.log(connector.web3.utils.utf8ToHex('0x37bd087c6c5c1d7af667c81146ce0d70d78c826ad421ef5a4f0ca233deb0ca23'))
       // console.log(connector.eth.accounts.hashMessage('0xe5f897cd55b1481f0e3e18b76d6d83ea8109949c2fc63a46f30366b9130fcec0000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000033030300000000000000000000000000000000000000000000000000000000000'))
-      // this.tx = await contract.makeProgrammable(this.contractAddress, this.tokenOfOwnerByIndex)
+      this.tx = await contract.makeProgrammable(this.contractAddress, this.tokenOfOwnerByIndex)
     }
   }
 }
